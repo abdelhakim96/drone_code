8f3f4fb0235499f0c4120aadbcb0d8af264265d5
Hakim <abdelhakim@aucegypt.edu>
2022-07-31 16:28:50 +0200

    opencv2

--- dji_m100_trajectory/src/GP_statemachine.cpp
+++ dji_m100_trajectory/src/GP_statemachine.cpp
@@ -13,7 +13,7 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PointStamped.h>
 //#include <mavros_msgs/CommandBool.h>
